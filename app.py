9664cdb271799ee8be81bc73246e9555ae8bee56
HelgeEgil <helge.e.s.pettersen@gmail.com>
2024-12-25 20:16:52 +0100

    Lagt til filer, men kan ikke kjøre på laptop

---
+++ app.py
@@ -0,0 +1,114 @@
+﻿from dash import Dash, html, dcc, dash_table
+from dash.dependencies import Input, Output
+import dash_bootstrap_components as dbc
+from callbacks import get_callbacks
+from flask import Flask
+from flask.logging import default_handler
+import logging
+
+logging.basicConfig(filename="app.log", 
+			filemode='a', 
+			format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                    	datefmt='%Y-%m-%d %H:%M:%S',
+                    	level=logging.INFO)
+
+server = Flask(__name__)
+
+app = Dash(
+	server=server, 
+	external_stylesheets=[
+		dbc.themes.FLATLY, 
+		dbc.icons.FONT_AWESOME,
+		css], 
+	suppress_callback_exceptions=True,  
+	url_base_pathname='/', 
+	serve_locally=True
+)
+
+get_callbacks()
+
+app.title = "Sigrid og Helge sitt kodeprogram"
+server = app.server
+
+for logger in (app.logger,):
+	logger.setLevel(logging.INFO)
+	logger.addHandler(default_handler)
+
+input_style = {'margin-left': '5px'}
+
+controls_left = dbc.Card([
+	html.Div(
+	    [
+	        dbc.Label("Skriv inn teksten som skal kodes"),
+	        dbc.Input(id="input", size="lg", placeholder="...", type="text"),
+
+	    ]
+	),
+	dbc.Label("Velg kodetype", style={"margin-top": "15px"}),
+	dbc.ButtonGroup(
+				[
+					dbc.RadioItems(
+						id="valg",
+						className="btn-group",
+						inputClassName="btn-check btn-lg",
+						labelClassName="btn btn-outline-primary btn-lg",
+						labelCheckedClassName="active",
+						options=[ {"label": k, "value": k} for k in ["Ingen", "Caesar", "Dobbel Caesar"] ],
+						value="Ingen"
+					),
+				],
+				className="radio-group",
+			),
+	html.Div(
+	    [
+	        dbc.Label("Første nøkkel"),
+	        dbc.Input(id="key1", size="lg", placeholder="...", type="text", options={"disabled": True}),
+
+	    ]
+	),
+	html.Div(
+	    [
+	        dbc.Label("Andre nøkkel"),
+	        dbc.Input(id="key2", size="lg", placeholder="...", type="text", options={"disabled": True}),
+
+	    ]
+	),
+    dbc.Switch(
+        id="retning",
+        label="Kode eller dekode?",
+        value=False,
+    ),
+    ],
+	body=True
+)
+
+controls_right = dbc.Card(
+	[
+		dbc.Label("Resultat:"),
+        dbc.Textarea(
+            invalid=True, size="lg", placeholder="...", id="result"
+        ),
+    ],
+	body=True,
+)
+
+app.layout = html.Div([
+	dbc.Col(html.H1("Sigrid og Helge sitt kodeprogram", className="text-center"), md=9),
+	dbc.Row(
+		[
+			dbc.Col(
+				dbc.CardGroup(
+					[
+						controls_left,
+		   				controls_right
+					],
+				),
+				md=9
+			)
+	   		
+		],
+	)
+])
+
+if __name__ == '__main__':
+	app.run_server(debug=True)
---
+++ callbacks.py
@@ -0,0 +1,52 @@
+﻿from dash import Dash, html, dcc, dash_table
+from dash.dependencies import Input, Output
+import dash_bootstrap_components as dbc
+import logging
+import datetime
+import string
+
+alphabet = "abcdefghijklmnopqrstuvwyxæøå"
+alphabet += alphabet
+alphabet_upper = alphabet.upper()
+
+logger = logging.getLogger("callbacks.py")
+	@app.callback([Output("key1", "options")],[Output("key2", "options"), Input("valg", "value")]):
+	def set_options_disabled(valg):
+		if valg == "Ingen":
+			return [{'disabled': True}], [{'disabled': True}]
+		elif valg == "Caesar":
+			return [{'disabled': False}], [{'disabled': True}]
+		else:
+			return [{'disabled': False}], [{'disabled': False}]
+
+	@app.callback(Output("resultat", "value"), 
+		[Input("valg", "value"), Input("key1", "value"), Input("key2", "value"), Input("text_input", "value"), Input("retning", "value")])
+	def encode(valg, key1, key2, text_input, retning):
+		if valg == "Ingen":
+			return text_input
+		if valg == "Caesar":
+			new_string = ""
+
+			for bokstav in text_input:
+				if bokstav in alphabet:
+					new_string = alphabet[alphabet.index() + key1]
+				elif bokstav in alphabet_upper:
+					new_string = alphabet_upper[alphabet_upper.index() + key1]
+				else:
+					new_string += bokstav
+
+			return new_string
+
+		if valg == "Dobbel Caesar":
+			idx = 0
+			keys = [key1, key2]
+
+			for bokstav in text_input:
+				if bokstav in alphabet:
+					new_string = alphabet[alphabet.index() + keys[idx%2]]
+				elif bokstav in alphabet_upper:
+					new_string = alphabet_upper[alphabet_upper.index() + keys[idx%2]]
+				else:
+					new_string += bokstav
+				idx += 1
+			return new_string
